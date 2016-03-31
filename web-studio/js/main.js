26becd7651dca8a0ec323096c92fcb37f85f93b1
serjo96 <promlemboy@gmail.com>
2016-03-31 18:54:35 +0500

    last fixes magic line and active menu

--- web-studio/js/main.js
+++ web-studio/js/main.js
@@ -37,3 +37,33 @@ $(window).scroll(function() {
         $('nav').removeClass('visible');
     }
 });
+
+$(function() {
+
+    var $el, leftPos, newWidth,
+        $mainNav = $("#nav_menu");
+
+    $mainNav.append("<li id='magic-line'></li>");
+    var $magicLine = $("#magic-line");
+
+    $magicLine
+        .width($(".current_page_item").width())
+        .css("left", $(".current_page_item a").position().left)
+        .data("origLeft", $magicLine.position().left)
+        .data("origWidth", $magicLine.width());
+
+    $("#nav_menu li a").hover(function() {
+        $el = $(this);
+        leftPos = $el.position().left;
+        newWidth = $el.parent().width();
+        $magicLine.stop().animate({
+            left: leftPos,
+            width: newWidth
+        });
+    }, function() {
+        $magicLine.stop().animate({
+            left: $magicLine.data("origLeft"),
+            width: $magicLine.data("origWidth")
+        });    
+    });
+});
