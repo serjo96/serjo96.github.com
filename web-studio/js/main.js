fc290871ccd3df480396a3fd0a1267be65459b69
serjo96 <promlemboy@gmail.com>
2016-03-31 14:40:47 +0500

    new scroll menu js

--- web-studio/js/main.js
+++ web-studio/js/main.js
@@ -1,9 +1,42 @@
-$(document).ready(function(){
-    $('nav ul li a').click(function(){
-        $('li a').removeClass("active");
-        $(this).addClass("active");
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('nav ul li a').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
     });
 });
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('nav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('nav ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
 $(window).scroll(function() {
     if ($(this).scrollTop() > 800) {
         $('nav').addClass('visible');
