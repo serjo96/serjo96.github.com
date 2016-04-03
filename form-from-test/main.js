4bb8bb35d5c931f4dae91502f61ad2c25ab88950
serjo96 <promlemboy@gmail.com>
2016-04-03 17:39:42 +0500

    new-fixes and support bg in ie8

--- form-from-test/main.js
+++ form-from-test/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $("#trigger-fild").click(function () {
-        $(this).closest('.form-section').find('form').slideToggle("fast").parent().find('form');
+        $(this).closest('.form-section').find('.form-cover').slideToggle("fast").parent().find('.form-cover');
     });  
 });
