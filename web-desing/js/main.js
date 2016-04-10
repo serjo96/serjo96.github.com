c80023c92706c26c73fe4531640a340f44fb8c18
serjo96 <promlemboy@gmail.com>
2016-04-10 17:53:38 +0500

    create site

---
+++ web-desing/js/main.js
@@ -0,0 +1,21 @@
+//$(document).ready(function() {
+//  var options = { videoId: '_ziUhNerFMI', 
+//                 ratio: 16/9 ,// usually either 4/3 or 16/9 -- tweak as needed
+//                 mute: true,
+//                 repeat: true,
+//                 playButtonClass: 'tubular-play',
+//                 pauseButtonClass: 'tubular-pause',
+//                 muteButtonClass: 'tubular-mute',
+//                 volumeUpClass: 'tubular-volume-up',
+//                 volumeDownClass: 'tubular-volume-down',
+//                 increaseVolumeBy: 10, // increment value; volume range is 1-100
+//                 start: 0, 
+//                } // starting position in seconds};
+//  
+//  $('#bg-site').tubular(options);
+//  // f-UGhWj1xww cool sepia hd
+//  // 49SKbS7Xwf4 beautiful barn sepia
+//});
+//$(function(){
+//  $("#bg-site").YTPlayer();
+//});
