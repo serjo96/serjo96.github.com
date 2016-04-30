08582d294a3ecdc861e2548a6c7a5365edd685cc
serjo96 <promlemboy@gmail.com>
2016-04-30 16:10:50 +0500

    new functuon

--- matrix/js/style.js
+++ matrix/js/style.js
@@ -8,3 +8,21 @@ $('.str_inp input').focus(function () {
 $(document).on("click",".clear-mtrx", function(){ 
   $(".str_inp input").val(""); 
 });
+
+$(document).on("click",".change-mtrx", function(){
+ 
+  $('.matrix_a').appendTo('.matrix_b_cover');
+  $(".matrix_b").appendTo(".matrix_a_cover");
+});
+
+
+//cheked radiobutton
+ $(document).ready(function(){
+    var rad=document.getElementsByName('mtrx_sel');
+    for (var i=0;i<rad.length; i++) {
+      if (rad[i].checked) {
+        alert('Выбран '+i+' radiobutton');
+      }
+    }
+
+});
