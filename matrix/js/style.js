3a8dffbc05afc033922b0ffe6f42d38565e9ee19
serjo96 <promlemboy@gmail.com>
2016-05-01 17:41:25 +0500

    add new functions add_str and del_str

--- matrix/js/style.js
+++ matrix/js/style.js
@@ -10,19 +10,41 @@ $(document).on("click",".clear-mtrx", function(){
 });
 
 $(document).on("click",".change-mtrx", function(){
- 
-  $('.matrix_a').appendTo('.matrix_b_cover');
-  $(".matrix_b").appendTo(".matrix_a_cover");
+  if ($(this).hasClass('flipped')) {
+    $('.matrix_a').appendTo('.matrix_a_cover');
+    $(".matrix_b").appendTo(".matrix_b_cover");
+  } else {
+    $('.matrix_a').appendTo('.matrix_b_cover');
+    $(".matrix_b").appendTo(".matrix_a_cover");
+  }
+  $(this).toggleClass('flipped');
 });
-
 
 //cheked radiobutton
- $(document).ready(function(){
-    var rad=document.getElementsByName('mtrx_sel');
-    for (var i=0;i<rad.length; i++) {
-      if (rad[i].checked) {
-        alert('Выбран '+i+' radiobutton');
-      }
-    }
-
+// $(".add_str").click(function(){
+//    var rad=document.getElementsByName('mtrx_sel');
+//    for (var i=0;i<rad.length; i++) {
+//      if (rad[i].checked) {
+//        alert('Выбран '+i+' radiobutton');
+//      }
+//    }
+//
+//});
+$('.add_str').click(function(){
+  if ($("#check_mtrx_a").prop("checked")){
+    $('.matrix_a tr:first').clone().appendTo('.matrix_a'); 
+  }else if($("#check_mtrx_b").prop("checked")) {
+    $('.matrix_b tr:first').clone().appendTo('.matrix_b'); 
+  }
 });
+
+if ($('#matrix_a tr').length > 2) {
+  $('.del_str').removeAttr("disabled").click(function(){
+    if ($("#check_mtrx_a").prop("checked")){
+      $('.matrix_a tr:last').remove();  
+    }else if($("#check_mtrx_b").prop("checked")) {
+      $('.matrix_b tr:last').remove();  
+    }
+  });
+} 
+
