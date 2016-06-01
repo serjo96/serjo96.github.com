2611eba40c4b980711ebcab5982b739a6726e553
serjo96 <promlemboy@gmail.com>
2016-06-01 18:13:53 +0500

    add new sections

--- zakaz-biletov/js/main.js
+++ zakaz-biletov/js/main.js
@@ -2,3 +2,16 @@ $(document).on('click', '.pass_select', function(show_pass){
   $('.select_block').toggleClass('visible');
                });
 
+$(document).on('click', '.price_tik_tab', function(){
+      $('.price_tik_tab a').addClass('active');
+      $('.pop_direc_tab a').removeClass('active');
+      $('.popular_direct').removeClass('visible');
+      $('.price_ticket').addClass('visible');
+});
+
+$(document).on('click', '.pop_direc_tab', function(){
+      $('.pop_direc_tab a').addClass('active');
+      $('.price_tik_tab a').removeClass('active');
+      $('.price_ticket').removeClass('visible');
+      $('.popular_direct').addClass('visible');
+});
