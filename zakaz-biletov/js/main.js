d2091022bc3a8b36463aac2fd0fd8bb608fbaf1a
serjo96 <promlemboy@gmail.com>
2016-06-03 19:04:42 +0500

    fixes circle on header and add graph

--- zakaz-biletov/js/main.js
+++ zakaz-biletov/js/main.js
@@ -17,31 +17,14 @@ $(document).on('click', '.pop_direc_tab', function(){
 });
 
 
-$(function () {
-  "use strict";
+$('#hov_trig').mouseover (function show_sidebar()
+{
+  console.log('see me')
+  document.getElementById('hov_block').style.display="block";
+});
 
-  var resizeTracker;
-
-  // Counteracts all transforms applied above an element.
-  // Apply a translation to the element to have it remain at a local position
-  var unscale = function (el) {
-    var svg = el.ownerSVGElement.ownerSVGElement;
-    var xf = el.scaleIndependentXForm;
-    if (!xf) {
-      // Keep a single transform matrix in the stack for fighting transformations
-      xf = el.scaleIndependentXForm = svg.createSVGTransform();
-      // Be sure to apply this transform after existing transforms (translate)
-      el.transform.baseVal.appendItem(xf);
-    }
-    var m = svg.getTransformToElement(el.parentNode);
-    m.e = m.f = 0; // Ignore (preserve) any translations done up to this point
-    xf.setMatrix(m);
-  };
-
-  [].forEach.call($("text, .tick"), unscale);
-
-  $(window).resize(function () {
-    if (resizeTracker) clearTimeout(resizeTracker);
-    resizeTracker = setTimeout(function () { [].forEach.call($("text, .tick"), unscale); }, 100);
-  });
-})
+$('#hov_trig').mouseout (function hide_sidebar()
+{
+  console.log("dont see me")
+  document.getElementById('hov_block').style.display="none";
+});
