a207827dfaf8d1f08a7ced2d7b3a9a8d8efc70f4
serjo96 <promlemboy@gmail.com>
2016-09-04 01:21:51 +0500

    new pages

--- jeneva/js/main.js
+++ jeneva/js/main.js
@@ -1,7 +1,26 @@
+//search fild
 $(document).on('focusin', '.search-btn', function(){
   $('.search-btn').addClass('focus-fild');
 });
 
 $(document).on('focusout', '.search-btn', function(){
   $('.search-btn').removeClass('focus-fild');
+});
+
+
+//delivery toggle
+
+$(document).on('click', '.js-pay-toggle', function(){
+  $('.pay').addClass('active-category');
+  $('.delivery').removeClass('active-category');
+ 
+});
+
+$(document).on('click', '.js-delivery-toggle', function(){
+  $('.delivery').addClass('active-category');
+  $('.pay').removeClass('active-category');
+});
+
+$(document).on('click', '.js-delivery-more', function(){
+  $('.dilevery-list').slideToggle();
 });
