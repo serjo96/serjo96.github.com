2b9bfc79674bc3f52cf31a6b856ef8dc3a87c630
Serjo96 <king-beer@yandex.ru>
2017-03-27 18:54:22 +0500

    13

--- study/dz7/src/cookie.js
+++ study/dz7/src/cookie.js
@@ -39,6 +39,9 @@ let addValueInput = homeworkContainer.querySelector('#add-value-input');
 let addButton = homeworkContainer.querySelector('#add-button');
 let listTable = homeworkContainer.querySelector('#list-table tbody');
 
+
+
+
 /**
  * Функция должна проверять встречается ли подстрока chunk в строке full
  * Проверка должна происходить без учета регистра символов
@@ -52,8 +55,7 @@ let listTable = homeworkContainer.querySelector('#list-table tbody');
  *
  * @return {boolean}
  */
-function isMatching(full, chunk) {
-}
+
 
 /**
  * Создает новый tr для таблицы со списком cookie
@@ -62,11 +64,134 @@ function isMatching(full, chunk) {
  * @param value - значение cookie
  */
 function createCookieTr(name, value) {
+    var str = document.createElement('tr');
+    var tdName = document.createElement('td');
+    var tdValue = document.createElement('td');
+    var tdDelete = document.createElement('td');
+    var btnDelite = document.createElement('button');
+
+
+
+    var filtr = filterNameInput.value;
+   if (isMatching(name,filtr) || isMatching (value,filtr)){
+       tdName.textContent = name;
+       tdValue.textContent = value;
+       btnDelite.textContent = 'Удалить cookie';
+       btnDelite.className = 'delete';
+       btnDelite.dataset.name = name;
+       tdDelete.appendChild(btnDelite);
+
+       str.appendChild(tdName);
+       str.appendChild(tdValue);
+       str.appendChild(tdDelete);
+       listTable.appendChild(str);
+   } else if(filtr.length == 0){
+        tdName.textContent = name;
+        tdValue.textContent = value;
+        btnDelite.textContent = 'Удалить cookie';
+        btnDelite.className = 'delete';
+        btnDelite.dataset.name = name;
+        tdDelete.appendChild(btnDelite);
+
+        str.appendChild(tdName);
+        str.appendChild(tdValue);
+        str.appendChild(tdDelete);
+        listTable.appendChild(str);
+    }
+
+}
+
+
+function isMatching(full, chunk) {
+    full = full.toLowerCase();
+    chunk = chunk.toLowerCase();
+    if (full.indexOf(chunk) == -1) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function allCookie () {
+    listTable.innerHTML = '';
+    var arrCookie = getCookie();
+    for(var i in arrCookie){
+        createCookieTr( i , arrCookie[i]);
+    }
+
+
+}
+
+
+function createCookie(name, value) {
+    document.cookie = name + '=' + value;
+}
+
+function deleteCookie(name) {
+    var now = new Date(0);
+    document.cookie = name + '=;' + "expires=" + now.toUTCString();
+}
+
+
+function getCookie () {
+    return document.cookie
+        .split('; ')
+        .filter(Boolean)
+        .map(cookie => cookie.match(/^([^=]+)=(.+)/))
+        .reduce((obj, [, name, value]) => {
+            obj[name] = value;
+
+            return obj;
+        }, {});
+}
+
+function refresh (string) {
+     string.childNodes[1].innerHTML = addValueInput.value;
+}
+
+
+function filterCheck () {
+    var filtr = filterNameInput.value;
+    for(var i = 0; i<listTable.childNodes.length; i++){
+        if( isMatching(listTable.childNodes[i].firstChild.innerHTML,filtr) ||  isMatching(listTable.childNodes[i].childNodes[1].innerHTML,filtr)) {
+        }else{
+            console.log(listTable.childNodes[i])
+            listTable.childNodes[i].remove();
+        }
+
+    }
+    console.log('work')
 }
 
 filterNameInput.addEventListener('keyup', function() {
+    allCookie();
+    filterCheck();
+
 });
 
 addButton.addEventListener('click', () => {
+    createCookie(addNameInput.value, addValueInput.value);
+    for(var i = 0; i<listTable.childNodes.length; i++){
+        if(listTable.childNodes[i].firstChild.innerHTML == addNameInput.value){
+            refresh(listTable.childNodes[i]);
+            filterCheck ();
+            return;
+        }
+    }
+    createCookieTr(addNameInput.value, addValueInput.value);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    allCookie();
+});
+
+
+listTable.addEventListener('click', (e) => {
+console.log(listTable.children.length);
+console.log(listTable.children)
+    if (e.target.className.indexOf('delete') > -1) {
+        deleteCookie(e.target.dataset.name);
+        e.target.parentNode.parentNode.remove();
+    }
 });
 
